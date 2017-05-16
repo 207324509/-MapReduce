2c05406285af28aa03d560e0bd9c8002365f7e5f
何发 <207324509@qq.com>
2017-05-16 22:34:22 +0800

    删除多余引用包

--- src/test/java/hadoop/mapreduce/MaxTemperatureTest.java
+++ src/test/java/hadoop/mapreduce/MaxTemperatureTest.java
@@ -21,7 +21,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputLogFilter;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
